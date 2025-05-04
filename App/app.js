e4da75d643196622657f8bb7dcfc614de5436a52
R.U.K.U.N.D.O <gihozoRukundobenise@gmail.com>
2025-05-04 23:04:10 +0200

    Updated project files and added new views

--- App/app.js
+++ App/app.js
@@ -3,6 +3,9 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import article from './models/article.js';
 import mongoose from 'mongoose';
+import { ExpressValidator } from 'express-validator';
+import flash from 'connect-flash';
+import session from 'express-session';
 
 //Connect to MongoDB
 mongoose.connect('mongodb://localhost/node');
@@ -27,7 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
 
 // Bring in models
 let Article = article;
@@ -54,21 +57,68 @@ app.set('view engine', 'pug');
 //     }
 // ]
 
+// Express Session Middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUnitialized: true,
+    cookie: { secure: true }
+}))
+
+//Express Messages Middleware
+app.use(require('connect-flash')());
+app.use((req, res, next) => {
+    res.locals.messages = require('express-messages')(req,res);
+    next();
+})
+
+// Express Validator Middleware
+app.use(ExpressValidator({
+    errorFormatter: (param, msg, value) => {
+        var namespace = param.split('.') 
+        , root = namespace.shift()
+        , formParam = root;
+
+        while(namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param : formParam,
+            msg : msg, 
+            value : value
+        }
+    }
+}))
+
 app.get('/', async (req, res) => {
     try {
         const articles = await Article.find({});
-        res.render('index', {
-            articles: articles
-    }); 
+        res.render('index', { activePage: 'home', articles }); // Pass articles to the view
     } catch (error) {
         console.log(error);
+        res.status(500).send('Server error');
     }
-})
+});
     // res.render('index', {
     //     articles: articles
     // });
 
-    app.get('/articles/add', (req,res) => {
+// Get Single Article
+app.get('/article/:id', async (req, res) => {
+    try {
+        const article = await Article.findById(req.params.id);
+        if (!article) {
+            return res.status(404).send('Article not found');
+        }
+        res.render('article', { article }); // Pass the article object to the view
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Server error');
+    }
+});
+ 
+
+app.get('/articles/add', (req,res) => {
         res.render('add_article', {
         })
     })
@@ -77,7 +127,7 @@ app.get('/', async (req, res) => {
     // app.post('/articles/add', (req, res) =>{
     //     console.log('Submitted');
     // })
-    app.post('/articles/add', async (req, res) => {
+app.post('/articles/add', async (req, res) => {
         try {
             let article = new Article();
             article.title = req.body.title;
@@ -86,13 +136,26 @@ app.get('/', async (req, res) => {
     
             await article.save(); // Use async/await instead of a callback
             res.redirect('/');
+            req.flash('success', 'Article Added')
         } catch (err) {
             console.log('Error saving article:', err);
             res.status(500).send('Failed to save the article');
         }
-    });
+});
 
-
+//Load Edit Form
+app.get('/article/edit/:id', async (req, res) => {
+    try {
+        const article = await Article.findById(req.params.id);
+        if (!article) {
+            return res.status(404).send('Article not found');
+        }
+        res.render('edit_article', { article }); // Pass the article object to the view
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Server error');
+    }
+});
 
 
 app.listen(3000, () => {console.log('Server started!')})
--- App/models/article.js
+++ App/models/article.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 //Article Schema
 let articleSchema = mongoose.Schema({
